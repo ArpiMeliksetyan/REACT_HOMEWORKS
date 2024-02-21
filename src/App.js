b76abb68ce75bf05d4651b62595a03df725fc1ee
ArpiMeliksetyan <arpine.meliksetyan@headspace.com>
2024-02-22 00:21:39 +0400

    Homework 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SearchForm from "./components/SearchForm";
+import Counter from "./components/Counter";
+import GenreSelect from "./components/GenreSelect";
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const listOfGenres = ['Comedy', 'Melodrama', 'Fantasy'];
+    const initialValue = 0;
+
+    function onSearch() {
+        console.log('Search');
+    }
+
+    function onSelect() {
+        console.log('Select');
+    }
+
+
+
+    return (
+        <div>
+            <Counter initialValue={initialValue}/>
+            <SearchForm initialSearchQuery='What do you want to watch?' onSearch={onSearch}/>
+            <GenreSelect genreNames={listOfGenres} selectedGenreName={listOfGenres[1]} onSelect={onSelect}/>
+        </div>
+    );
 }
 
 export default App;
